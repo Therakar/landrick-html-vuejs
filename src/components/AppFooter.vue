d96e593a653e4b24ca0e02a03ed1e904053703fb
Therakar <matteo.pau200@gmail.com>
2022-12-10 21:38:09 +0100

    aggiunta  footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,15 +1,34 @@
 <script>
+import AppFooterTop from './AppFooterComponents/AppFooterTop.vue';
+import AppFooterBottom from './AppFooterComponents/AppFooterBottom.vue'
     export default {
-        name: "AppFooter"
+        name: "AppFooter",
+        components: {
+            AppFooterTop,
+            AppFooterBottom
+        }
     }
 </script>
 
 <template>
-    <div>
-
-    </div>
+    <footer>
+        
+        <section id="container footer-top flex ">
+            <AppFooterTop />
+        </section>
+        <hr>
+        <section id="container footer-bottom flex ">
+            <AppFooterBottom />
+        </section>
+    </footer>
 </template>
 
 <style lang="scss" scoped>
-
+    footer{
+        background-color: var(--background-third-color);
+        height: 500px; //debug
+        .footer-top{
+            padding: 65px 0;
+        }
+    }
 </style>
---
+++ src/components/AppFooterComponents/AppFooterTop.vue
@@ -0,0 +1,16 @@
+<script>
+    export default {
+        name: "AppFooterTop"
+    }
+</script>
+
+<template>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
